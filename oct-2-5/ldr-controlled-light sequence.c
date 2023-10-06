5ab5a0e9831596e160c19fcdd2430bbc0d121739
Aman Gaur <aman.gaur1011@gmail.com>
2023-10-06 13:29:30 -0300

    fixing

--- oct-2-5/ldr-controlled-light sequence.c
+++ oct-2-5/ldr-controlled-light sequence.c
@@ -1,28 +1,35 @@
+
 int brightness = 0;
 int fadeAmount = 4;  // since Analog write values from 0-255, keeping fade as 4 gives 4*60 , values upto 240.
-int led = 5;
+int led = 6;
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
   //pinMode(led, OUTPUT);
   Serial.begin(9600);
+  analogWrite(led, 255);
+  delay(2000);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
   int photoResistorValue = analogRead(A0);
+  Serial.println("photoresistorvalue");
+  Serial.println(photoResistorValue);
+  brightness = 0;
 
-  //if (photoResistorValue < 45) {
+  if (photoResistorValue < 45) {
       int count = 0;
       while (count < 61) {
-        Serial.println(brightness);
         brightness = brightness + fadeAmount;
-        analogWrite(5, brightness);
+        analogWrite(led, brightness);
+        Serial.println("brightness");
+        Serial.println(brightness);
         delay(1000);
         count++;
       }
-  //}
+  }
 
   // Reset to turn off the light, once the sequence finishes.
   analogWrite(5,0);
